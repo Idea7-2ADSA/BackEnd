1d433ecb3ea1c240e9cd850c7e827b62ba912755
DanielRRSilva <daniel.rsilva@sptech.school>
2024-04-03 23:00:12 -0300

    login refatorado e funcinal

---
+++ login/src/main/java/SistemaMetodos.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SistemaMetodos {
+   private Map<String, Usuario> mapaDeUsuarios = new HashMap<>();
+   protected void adicionarUsuarioPadrao() {
+        String email = "daniel@gmail.com";
+        String senha = "daniel123";
+        Usuario usuario = new Usuario("Daniel", email, "11.111.111/1111-11", senha);
+        mapaDeUsuarios.put(email, usuario);
+   }
+
+   public Boolean validarEmailLogin(String email) {
+    for (Map.Entry<String, Usuario> usuario: mapaDeUsuarios.entrySet()) {
+        if (usuario.getKey().equals(email)) {
+            return true;
+        }
+    }
+    System.out.println("Usuário não encontrado!");
+    return false;
+   }
+
+   public Boolean validarSenhaLogin(String senha) {
+        for (Map.Entry<String, Usuario> usuario: mapaDeUsuarios.entrySet()) {
+            if (usuario.getValue().getSenha().equals(senha)) {
+                return true;
+            }
+        }
+        System.out.println("Senha inválida!");
+        return false;
+   }
+}
--- login/src/main/java/Usuario.java
+++ login/src/main/java/Usuario.java
@@ -3,41 +3,54 @@ public class Usuario {
     private String email;
     private String senha;
     private  String cnpj;
-    public void usuario(String nome, String email, String cnpj, String senha) {
+
+    public Usuario(String nome, String email, String cnpj, String senha) {
         this.nome = nome;
         this.email = email;
         this.cnpj = cnpj;
         this.senha = senha;
     }
+
+    public Usuario() {
+    }
+    
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public  void setEmail(String email) {
         this.email = email;
     }
+
     public  void setSenha(String senha) {
         this.senha = senha;
     }
+
     public void setCnpj(String cnpj) {
         this.cnpj = cnpj;
     }
+
     public String getNome() {
         return this.nome;
     }
+
     public String getEmail() {
         return this.email;
     }
+
     public String getCnpj() {
         return this.cnpj;
     }
+
     public String getSenha() {
         return this.senha;
     }
-    public String getUsuario() {
+
+    public String toString() {
         return String.format("""
                 nome: %s
                 email: %s
                 cnpj: %s
-                """, this.nome, this.email, this.senha);
+                """, this.nome, this.email, this.cnpj);
     }
 }
